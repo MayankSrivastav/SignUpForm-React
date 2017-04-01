7135081d6ddf5ae2933e376b5fc592efe1be92ba
MayankSrivastav <mayanksri18@yahoo.in>
2017-04-01 22:02:42 +0530

    Created main form. Modal component

--- js/index.js
+++ js/index.js
@@ -12,3 +12,27 @@ var Input = React.createClass({
       );
    }
 });
+
+var Modal = React.createClass({
+   displayName: "Modal",
+
+   render: function render() {
+      return React.createElement(
+         "div",
+         { className: "Modal" },
+         React.createElement(
+            "form",
+            { onSubmit: this.props.onSubmit, className: "ModalForm" },
+            React.createElement(Input, { id: "name", type: "text", placeholder: "Mayank Srivastav" }),
+            React.createElement(Input, { id: "username", type: "email", placeholder: "mayanksri18@yahoo.in" }),
+            React.createElement(Input, { id: "password", type: "password", placeholder: "password" }),
+            React.createElement(
+               "button",
+               null,
+               "Log in ",
+               React.createElement("i", { className: "fa fa-fw fa-chevron-right" })
+            )
+         )
+      );
+   }
+});
