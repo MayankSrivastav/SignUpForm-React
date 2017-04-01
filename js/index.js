4148241d3406ba3427022137d1df54c80162ef85
MayankSrivastav <mayanksri18@yahoo.in>
2017-04-01 22:01:23 +0530

    created Input component. Handles  input element

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
 "use strict";
+
+var Input = React.createClass({
+   displayName: "Input",
+
+   render: function render() {
+      return React.createElement(
+         "div",
+         { className: "Input" },
+         React.createElement("input", { id: this.props.name, type: this.props.type, placeholder: this.props.placeholder }),
+         React.createElement("label", { "for": this.props.name })
+      );
+   }
+});
